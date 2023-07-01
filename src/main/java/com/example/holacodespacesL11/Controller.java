fc42fa60398f32af7a582698e12e20b378d14c40
8Lozano8 <112447324+8Lozano8@users.noreply.github.com>
2023-07-01 21:24:18 +0000

    cambios de prueba

---
+++ src/main/java/com/example/holacodespacesL11/Controller.java
@@ -0,0 +1,18 @@
+package com.example.holacodespacesL11;
+
+import java.lang.String;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping(path="/")
+public class Controller{
+
+    @GetMapping(path="/")
+    public String home(){
+        return "AT75969192 - Jean Carlos Lozano Curi";
+    }
+
+}
